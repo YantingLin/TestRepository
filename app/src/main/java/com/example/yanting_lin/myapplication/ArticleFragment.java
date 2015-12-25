202590a022de2c7a643bec2b445dffc5c08bd052
Kobayashilin <ss61418@gmail.com>
2015-12-25 17:21:38 +0800

    [local]Week 2 Training 1. One fragment is defined in XML and another dynammic created 2. Two fragments can reset another's textBox with itself editbox 3. Modified coding style

--- app/src/main/java/com/example/yanting_lin/myapplication/ArticleFragment.java
+++ app/src/main/java/com/example/yanting_lin/myapplication/ArticleFragment.java
@@ -6,25 +6,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
  * ForSecondActivityUsed
  * Created by yanting_lin on 2015/12/22.
- *
  */
 public class ArticleFragment extends Fragment {
-    final static String ARG_POSITION = "position";
+
     final static String TAG_ARTICLE_FRAGMENT = "ArticleFragment";
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        super.onCreateView(inflater,container,savedInstanceState);
-        Log.v(TAG_ARTICLE_FRAGMENT, "FragmentCreate");
+        super.onCreateView(inflater, container, savedInstanceState);
 
-        return  inflater.inflate(R.layout.article_fragment,container,false);
+        Log.v(TAG_ARTICLE_FRAGMENT, "FragmentCreate");
+        return inflater.inflate(R.layout.article_fragment, container, false);
+
     }
-    public void updateArticleView(int position) {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public void updateArticleView(String inputText) {
         TextView article = (TextView) getActivity().findViewById(R.id.textView);
-        article.setText("傳值：" + position);
+        article.setText(inputText);
     }
+
+    public String getEditText() {
+        EditText editText = (EditText) getActivity().findViewById(R.id.editText);
+        return editText.getText().toString();
+    }
+
 }
--- app/src/main/java/com/example/yanting_lin/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/yanting_lin/myapplication/SecondActivity.java
@@ -1,24 +1,28 @@
 package com.example.yanting_lin.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.Button;
 
+/**
+ * 承接 2015/12/24 ContentFragment.java的註解
+ * 原先要implement ContentFragment.OnContentSelectedListener
+ */
 
-public class SecondActivity extends FragmentActivity implements View.OnClickListener,ContentFragment.OnContentSelectedListener  {
+public class SecondActivity extends FragmentActivity implements View.OnClickListener {
 
-    private static String SECOND_ACTIVITY_TAG = "SecondActivity";
-    private static String KEY_PUT_DATA_NAME = "ResponseFromSecondActivity";
-    private static int ARTICLE_FRAGMENT_ID = 0;
+    private static final String SECOND_ACTIVITY_TAG = "SecondActivity";
+    private static final String KEY_PUT_DATA_NAME = "ResponseFromSecondActivity";
+    private static boolean IS_CONTENT_BUTTON_SET = false;
+    //動態產生的Fragment才需要
+    private static final String TAG_FOR_FRAGMENT_CONTENT_CREATED = "ContentFragment";
+    Button ContentFragmentButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +31,20 @@ public class SecondActivity extends FragmentActivity implements View.OnClickList
         Log.v(SECOND_ACTIVITY_TAG, "(1) OverrideOnCreate");
         Intent intent = getIntent();
         //取得MainActivity單方傳送之資料
-        Log.v(SECOND_ACTIVITY_TAG, "DataFromMainActivity："+ intent.getExtras()
+        Log.v(SECOND_ACTIVITY_TAG, "DataFromMainActivity：" + intent.getExtras()
                 .getString("ValueFromMainActivity"));
-        //資料放置後，直接結束回傳。
+        //資料放置後，直接結束回傳
         Bundle bundle = new Bundle();
         bundle.putString(KEY_PUT_DATA_NAME, "ABCDEFG");
         intent.putExtras(bundle);
         setResult(753, intent);
-       // finish();
 
         //Fragment Change Action
         final Button changeFragmentButton = (Button) findViewById(R.id.change_button);
         changeFragmentButton.setOnClickListener(this);
 
-        //Fragment Dynamically Create
-        if(findViewById(R.id.fragment_container)!=null){
-            if(savedInstanceState != null){
-                return;
-            }
-            ArticleFragment firstFragment = new ArticleFragment();
-            firstFragment.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
-
-            ContentFragment secondFragment = new ContentFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, secondFragment).commit();
-            Log.v(SECOND_ACTIVITY_TAG, secondFragment.getId() + "Content_FRAGMENT_ID's Value");
-        }
+        final Button ArticleFragmentButton = (Button) findViewById(R.id.article_button);
+        ArticleFragmentButton.setOnClickListener(this);
 
     }
 
@@ -65,80 +55,99 @@ public class SecondActivity extends FragmentActivity implements View.OnClickList
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.v(SECOND_ACTIVITY_TAG, "(3) OverrideOnResume");
     }
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.v(SECOND_ACTIVITY_TAG, "(4) OverrideOnPause");
     }
+
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.v(SECOND_ACTIVITY_TAG, "(5) OverrideOnStop");
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.v(SECOND_ACTIVITY_TAG, "(6) OverrideOnDestroy");
     }
+
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.v(SECOND_ACTIVITY_TAG, "(7) OverrideOnSaveInstanceState");
     }
 
 
-    public void onClick(View view){
-//        switch (){
-//           case R.layout.article_fragment:
-                //Replace firstFragment
+    public void onClick(View view) {
+        Log.v(SECOND_ACTIVITY_TAG, "onClick event happened");
+
+        switch (view.getId()) {
+            case R.id.change_button: {
+                //Add second Fragment
+                Log.v(SECOND_ACTIVITY_TAG, "Get change button!");
                 ContentFragment secondFragment = new ContentFragment();
-                FragmentTransaction SecondTransaction = getSupportFragmentManager().beginTransaction();
-                SecondTransaction.replace(R.id.fragment_container,secondFragment);
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction SecondTransaction = fragmentManager
+                        .beginTransaction();
+                SecondTransaction.add(R.id.vertical_layout_of_second_activity, secondFragment,
+                        TAG_FOR_FRAGMENT_CONTENT_CREATED);
                 SecondTransaction.addToBackStack(null);
                 SecondTransaction.commit();
-//                break;
-//            case R.layout.content_fragment:
-//                //Replace secondFragment
-//                ArticleFragment firstFragment = new ArticleFragment();
-//                FragmentTransaction firstTransaction = getSupportFragmentManager().beginTransaction();
-//                firstTransaction.replace(R.id.fragment_container,firstFragment);
-//                firstTransaction.addToBackStack(null);
-//                firstTransaction.commit();
-//                break;
-//        }
+                //可以同步
+                fragmentManager.executePendingTransactions();
 
-    }
+                //Content Button Event Initialization
+                if (!IS_CONTENT_BUTTON_SET && getSupportFragmentManager()
+                        .findFragmentByTag(TAG_FOR_FRAGMENT_CONTENT_CREATED) != null) {
 
-    public void onArticleSelected(int position){
-        //Check ArticleFragment exist or not
-        //有Try Catch就可以傳送資料
-        //Log.v(SECOND_ACTIVITY_TAG, getSupportFragmentManager().getFragments().get(0));
+                    ContentFragmentButton = (Button) getSupportFragmentManager()
+                            .findFragmentByTag(TAG_FOR_FRAGMENT_CONTENT_CREATED).getView()
+                            .findViewById(R.id.content_button);
 
-        ArticleFragment checkFragment = (ArticleFragment) getSupportFragmentManager()
-                .getFragments().get(0);
+                    if (ContentFragmentButton != null) {
+                        ContentFragmentButton.setOnClickListener(this);
+                        Log.v(SECOND_ACTIVITY_TAG, "Set Content button event");
+                        IS_CONTENT_BUTTON_SET = true;
+                    }
+                }
+            }
+            break;
+            case R.id.article_button: {
+                //ArticleFragment transfer data to ContentFragment
+                Log.v(SECOND_ACTIVITY_TAG, "Get article button!");
+                ArticleFragment articleFragment = (ArticleFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.article_Container);
+                if (articleFragment != null) {
+                    ContentFragment contentFragment = (ContentFragment) getSupportFragmentManager()
+                            .findFragmentByTag(TAG_FOR_FRAGMENT_CONTENT_CREATED);
+                    if (contentFragment != null) {
+                        contentFragment.updateContentView(articleFragment.getEditText());
+                    }
+                }
+            }
+            break;
+            case R.id.content_button: {
+                //ContentFragment transfer data to ArticleFragment
+                Log.v(SECOND_ACTIVITY_TAG, "Get content button!");
+                ContentFragment contentFragment = (ContentFragment) getSupportFragmentManager()
+                        .findFragmentByTag(TAG_FOR_FRAGMENT_CONTENT_CREATED);
 
-        if (checkFragment!=null){
-            checkFragment.updateArticleView(position);
-            Log.v(SECOND_ACTIVITY_TAG, "onArticleSelected&firstFragment!=NULL");
+                if (contentFragment != null) {
+                    ArticleFragment articleFragment = (ArticleFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.article_Container);
+
+                    if (articleFragment != null) {
+                        articleFragment.updateArticleView(contentFragment.getEditText());
+                    }
+                }
+            }
         }
-        else {
-            Log.v(SECOND_ACTIVITY_TAG,"firstFragment is NULL");
-            ArticleFragment newFragment = new ArticleFragment();
-            Bundle args = new Bundle();
-            args.putInt(ArticleFragment.ARG_POSITION, position);
-            newFragment.setArguments(args);
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment_container, newFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
     }
-
-
-
-
 }
